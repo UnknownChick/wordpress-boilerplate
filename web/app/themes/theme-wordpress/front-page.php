f0ddfc73128508b934c1340124fb804a1aee49d8
Alexandre <35708209+UnknownChick@users.noreply.github.com>
2024-02-27 02:03:16 +0100

    feat(theme): init

---
+++ web/app/themes/theme-wordpress/front-page.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+	<main>
+		<h1>Test</h1>
+	</main>
+<?php get_footer(); ?>
---
+++ web/app/themes/theme-wordpress/inc/shortcodes.php
@@ -0,0 +1,55 @@
+<?php
+defined('ABSPATH') || die();
+/**
+ * Initializes the shortcodes.
+ */
+function shortcodes_init() {
+	/**
+	 * Generates a site map.
+	 *
+	 * @param array $atts An array of attributes for the site map.
+	 * @return string The generated site map HTML.
+	 */
+	function site_map($atts) {
+		$pages = get_pages();
+		$posts = get_posts();
+		$authors = get_users();
+		$categories = get_categories();
+		
+		$site_map = '<ul class="'.$atts['class'].'">';
+		
+		if (isset($atts['type'])) {
+			switch ($atts['type']) {
+				case 'pages':
+					foreach ($pages as $page) {
+						$site_map .= '<li><a href="'.get_permalink($page->ID).'">'.$page->post_title.'</a></li>';
+					}
+					break;
+				case 'posts':
+					foreach ($posts as $post) {
+						$site_map .= '<li><a href="'.get_permalink($post->ID).'">'.$post->post_title.'</a></li>';
+					}
+					break;
+				case 'authors':
+					foreach ($authors as $author) {
+						$site_map .= '<li><a href="'.get_permalink($author->ID).'">'.$author->data->user_login.'</a></li>';
+					}
+					break;
+				case 'categories':
+					foreach ($categories as $categorie) {
+						$site_map .= '<li><a href="'.get_permalink($categorie->ID).'">'.$categorie->cat_name.'</a></li>';
+					}
+					break;
+				default:
+
+					break;
+			}
+		}
+		$site_map .= '</ul>';
+		
+		echo $site_map;
+	}
+	add_shortcode('site_map', 'site_map');
+}
+add_action('init', 'shortcodes_init');
+?>
