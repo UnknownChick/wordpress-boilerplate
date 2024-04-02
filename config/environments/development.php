d6ab818543a561a568c8ed4c3707e75644bad9ae
Alexandre <alexandre.ferreira7@proton.me>
2024-04-02 23:51:49 +0200

    feat(vite config): add env varaible for theme name

--- config/environments/development.php
+++ config/environments/development.php
@@ -14,6 +14,6 @@ Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
 
-Config::define('VITE_DEV_SERVER', env('VITE_DEV_SERVER'));
+define('VITE_DEV_SERVER', env('VITE_DEV_SERVER'));
 
 ini_set('display_errors', '1');
--- web/app/themes/theme-wordpress/inc/hmr.php
+++ web/app/themes/theme-wordpress/inc/hmr.php
@@ -1,28 +1,46 @@
-<?php
-function getViteDevServerAddress() {
-	return VITE_DEV_SERVER;
-}
+<?php defined('ABSPATH') || die();
 
-function isViteHMRAvailable() {
-	return !empty(getViteDevServerAddress());
+/**
+ * Retrieves the address of the Vite development server.
+ *
+ * @return string The address of the Vite development server.
+ */
+function getViteDevServerAddress(): string
+{
+    return VITE_DEV_SERVER;
 }
 
 /**
- * Loads given script as a EcmaScript module.
- * 
- * @param string $script_handle Name of the script to load as
- * module.
+ * Checks if the Vite Hot Module Replacement (HMR) is available.
+ *
+ * @return bool True if the Vite HMR is available, false otherwise.
+ */
+function isViteHMRAvailable(): bool
+{
+    return !empty(getViteDevServerAddress());
+}
+
+/**
+ * Loads a given JavaScript script as an ECMAScript module.
+ *
+ * This function uses the WordPress 'script_loader_tag' filter to modify the script tag of the
+ * specified script, changing its type to 'module'. This allows the script to be loaded as an
+ * ECMAScript module, which can import other modules using the 'import' keyword.
+ *
+ * @param string $script_handle The handle of the script to load as a module. This handle should
+ * correspond to a script that has been registered using wp_register_script() or wp_enqueue_script().
  *
  * @return void
  */
-function loadJSScriptAsESModule($script_handle) {
-	add_filter('script_loader_tag', function ($tag, $handle, $src) use ($script_handle) {
-		if ($script_handle === $handle) {
-			return sprintf(
-				'<script type="module" src="%s"></script>',
-				esc_url($src)
-			);
-		}
-		return $tag;
-	}, 10, 3);
+function loadJSScriptAsESModule(string $script_handle): void
+{
+    add_filter('script_loader_tag', function ($tag, $handle, $src) use ($script_handle) {
+        if ($script_handle === $handle) {
+            return sprintf(
+                '<script type="module" src="%s"></script>',
+                esc_url($src)
+            );
+        }
+        return $tag;
+    }, 10, 3);
 }
