adf7c84e198a80a57648e33c96aa072ea56408f0
Alexandre <35708209+UnknownChick@users.noreply.github.com>
2024-09-29 19:04:01 +0200

    refactor(functions.php): separate into multiple files

---
+++ web/app/themes/theme-wordpress/inc/core.php
@@ -0,0 +1,33 @@
+<?php defined('ABSPATH') || die();
+
+// Menus
+register_nav_menus([
+    'main' => 'Main Menu',
+]);
+
+// Replace empty href attributes with span tags
+add_filter('wp_nav_menu', function ($nav_menu, $args) {
+    return preg_replace('/<a(?![^>]*href)([^>]*)>(.*?)<\/a>/', '<span$1>$2</span>', $nav_menu);
+}, 10, 2);
+
+// Remove <p> tags from cotent and excerpt
+add_filter('the_content', function ($content) {
+    remove_filter('the_content', 'wpautop');
+    remove_filter('the_excerpt', 'wpautop');
+    return $content;
+}, 0);
+
+// Limit excerpt length
+add_filter('excerpt_length', function ($length) {
+    return 40;
+});
+
+// Theme support
+add_theme_support('menus');
+add_theme_support('post-thumbnails');
+add_theme_support('title-tag');
+// add_theme_support('widgets');
+
+// Disable the block editor
+add_filter('use_block_editor_for_post', '__return_false', 10);
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
