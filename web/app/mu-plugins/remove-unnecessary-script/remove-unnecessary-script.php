77674994e0dbac4f3604672d1f0a7b9f584eaa7e
Alexandre <alexandre.ferreira7@proton.me>
2024-09-29 23:09:32 +0200

    feat(mu-plugins remove-unnecessary-script): remove more useless scripts

--- web/app/mu-plugins/remove-unnecessary-script/remove-unnecessary-script.php
+++ web/app/mu-plugins/remove-unnecessary-script/remove-unnecessary-script.php
@@ -32,14 +32,19 @@ add_action('wp_enqueue_scripts', function () {
  * Removes emojis from the website.
  */
 add_action('init', function () {
-    remove_action('admin_print_styles', 'print_emoji_styles');
-    remove_action('wp_head', 'print_emoji_detection_script', 7);
     remove_action('admin_print_scripts', 'print_emoji_detection_script');
     remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
     remove_action('wp_print_styles', 'print_emoji_styles');
     remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-    remove_filter('the_content_feed', 'wp_staticize_emoji');
-    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_action('wp_head', 'feed_links', 2);
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('wp_head', 'rest_output_link_wp_head');
+    remove_action('wp_head', 'wp_resource_hints', 2);
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
 });
 
 
@@ -52,9 +57,3 @@ add_action('wp_enqueue_scripts', function () {
     }
     wp_deregister_style('dashicons');
 });
-
-// Remove WLW Manifest link (Windows Live Writer)
-remove_action('wp_head', 'wlwmanifest_link');
-
-// Remove RSD (Really Simple Discovery) link
-remove_action('wp_head', 'rsd_link');
