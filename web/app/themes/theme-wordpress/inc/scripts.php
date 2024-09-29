e54bd3307bf21dd4fdc6c6761e2c31e039876b0c
UnknownChick <ferreira.alexandre.2000@outlook.com>
2024-09-29 23:13:09 +0200

    refactor(scripts.php): use native function to load script as module

--- web/app/themes/theme-wordpress/inc/scripts.php
+++ web/app/themes/theme-wordpress/inc/scripts.php
@@ -9,12 +9,9 @@ add_action('wp_enqueue_scripts', function () {
         $sass_handle = 'scss';
         $vite_handle = 'vite-client';
 
-        wp_enqueue_script($ts_handle, getViteDevServerAddress().'/assets/js/app.js', $dependencies, $version);
-        wp_enqueue_script($vite_handle, getViteDevServerAddress().'/@vite/client', $dependencies, $version);
+        wp_enqueue_script_module($ts_handle, getViteDevServerAddress().'/assets/js/app.js', $dependencies, $version);
+        wp_enqueue_script_module($vite_handle, getViteDevServerAddress().'/@vite/client', $dependencies, $version);
         wp_enqueue_style($sass_handle, getViteDevServerAddress().'/assets/scss/style.scss', $dependencies, $version);
-
-        loadJSScriptAsESModule($vite_handle);
-        loadJSScriptAsESModule($ts_handle);
     } else {
         wp_enqueue_style('style-theme', get_stylesheet_uri(), $dependencies, $version);
         wp_enqueue_style('style-dist', get_stylesheet_directory_uri().'/dist/style.css', $dependencies, $version);
