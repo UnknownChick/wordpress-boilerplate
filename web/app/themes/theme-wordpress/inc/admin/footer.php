84ecc021be033c170822377ead703d4ad3fa577d
UnknownChick <ferreira.alexandre.2000@outlook.com>
2024-10-01 22:29:40 +0200

    feat(admin): custom footer text

---
+++ web/app/themes/theme-wordpress/inc/admin/footer.php
@@ -0,0 +1,5 @@
+<?php defined('ABSPATH') || die();
+
+add_action('admin_footer_text', function (): void {
+   echo 'Developed by <a href="https://alexandre-ferreira.fr" target="_blank">Alexandre Ferreira</a>';
+});
