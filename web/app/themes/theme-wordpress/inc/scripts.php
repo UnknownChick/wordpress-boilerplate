696b1826faa9bfb8c6970bc60f632ce73286ef0d
UnknownChick <ferreira.alexandre.2000@outlook.com>
2024-10-03 22:21:25 +0200

    refactor(hrm + scripts): create class for HMR

--- web/app/themes/theme-wordpress/inc/scripts.php
+++ web/app/themes/theme-wordpress/inc/scripts.php
@@ -1,20 +1,24 @@
 <?php defined('ABSPATH') || die();
 
+use ThemeWordpress\Inc\HMR;
+
 add_action('wp_enqueue_scripts', function () {
+	$hmr = new HMR();
     $dependencies = array();
     $version = null;
 
-    if (!function_exists('isViteHMRAvailable') && isViteHMRAvailable()) {
+    if ($hmr->isHMRAvailable()) {
         $ts_handle = 'app';
         $sass_handle = 'scss';
         $vite_handle = 'vite-client';
 
-        wp_enqueue_script_module($ts_handle, getViteDevServerAddress().'/assets/js/app.js', $dependencies, $version);
-        wp_enqueue_script_module($vite_handle, getViteDevServerAddress().'/@vite/client', $dependencies, $version);
-        wp_enqueue_style($sass_handle, getViteDevServerAddress().'/assets/scss/style.scss', $dependencies, $version);
+        wp_enqueue_script($ts_handle, $hmr->getViteDevServerAddress().'/assets/js/app.js', $dependencies, $version);
+        wp_enqueue_script($vite_handle, $hmr->getViteDevServerAddress().'/@vite/client', $dependencies, $version);
+        wp_enqueue_style($sass_handle, $hmr->getViteDevServerAddress().'/assets/scss/style.scss', $dependencies, $version);
     } else {
         wp_enqueue_style('style-theme', get_stylesheet_uri(), $dependencies, $version);
         wp_enqueue_style('style-dist', get_stylesheet_directory_uri().'/dist/style.css', $dependencies, $version);
         wp_enqueue_script('js-dist', get_stylesheet_directory_uri().'/dist/app.js', $dependencies, $version, true);
     }
 });
+ 
