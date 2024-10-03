beb9b3e72cb975b549deafcd4e787128cad5ed01
Alexandre <alexandre.ferreira7@proton.me>
2024-10-03 22:21:25 +0200

    refactor(hrm + scripts): create class for HMR

--- web/app/themes/theme-wordpress/inc/hmr.php
+++ web/app/themes/theme-wordpress/inc/hmr.php
@@ -1,46 +1,65 @@
-<?php defined('ABSPATH') || die();
+<?php
+namespace ThemeWordpress\Inc;
+
+defined('ABSPATH') || die();
 
 /**
- * Retrieves the address of the Vite development server.
- *
- * @return string The address of the Vite development server.
+ * Class HMR
+ * 
+ * Handles Hot Module Replacement (HMR) functionality for Vite integration.
  */
-function getViteDevServerAddress(): string
-{
-    return VITE_DEV_SERVER;
-}
+class HMR {
+    private string $viteDevServer;
 
-/**
- * Checks if the Vite Hot Module Replacement (HMR) is available.
- *
- * @return bool True if the Vite HMR is available, false otherwise.
- */
-function isViteHMRAvailable(): bool
-{
-    return !empty(getViteDevServerAddress());
-}
+    /**
+     * HMR constructor.
+     */
+    public function __construct() {
+        $this->viteDevServer = VITE_DEV_SERVER;
+    }
 
-/**
- * Loads a given JavaScript script as an ECMAScript module.
- *
- * This function uses the WordPress 'script_loader_tag' filter to modify the script tag of the
- * specified script, changing its type to 'module'. This allows the script to be loaded as an
- * ECMAScript module, which can import other modules using the 'import' keyword.
- *
- * @param string $script_handle The handle of the script to load as a module. This handle should
- * correspond to a script that has been registered using wp_register_script() or wp_enqueue_script().
- *
- * @return void
- */
-function loadJSScriptAsESModule(string $script_handle): void
-{
-    add_filter('script_loader_tag', function ($tag, $handle, $src) use ($script_handle) {
-        if ($script_handle === $handle) {
-            return sprintf(
-                '<script type="module" src="%s"></script>',
-                esc_url($src)
-            );
-        }
-        return $tag;
-    }, 10, 3);
+    /**
+     * Retrieves the address of the Vite development server.
+     *
+     * @return string The address of the Vite development server.
+     */
+    public function getViteDevServerAddress(): string
+    {
+        return $this->viteDevServer;
+    }
+
+    /**
+     * Checks if the Vite Hot Module Replacement (HMR) is available.
+     *
+     * @return bool True if the Vite HMR is available, false otherwise.
+     */
+    public function isHMRAvailable(): bool
+    {
+        return !empty($this->viteDevServer);
+    }
+
+    /**
+     * Loads a given JavaScript script as an ECMAScript module.
+     *
+     * This function uses the WordPress 'script_loader_tag' filter to modify the script tag of the
+     * specified script, changing its type to 'module'. This allows the script to be loaded as an
+     * ECMAScript module, which can import other modules using the 'import' keyword.
+     *
+     * @param string $script_handle The handle of the script to load as a module. This handle should
+     * correspond to a script that has been registered using wp_register_script() or wp_enqueue_script().
+     *
+     * @return void
+     */
+    public function loadJSScriptAsESModule(string $script_handle): void
+    {
+        add_filter('script_loader_tag', function ($tag, $handle, $src) use ($script_handle) {
+            if ($script_handle === $handle) {
+                return sprintf(
+                    '<script type="module" src="%s"></script>',
+                    esc_url($src)
+                );
+            }
+            return $tag;
+        }, 10, 3);
+    }
 }
