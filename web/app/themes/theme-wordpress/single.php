e163c5b818bed3db2436a7313aac02197642fa7f
Alexandre <alexandre.ferreira7@proton.me>
2024-11-18 22:48:58 +0100

    feat(single): single template

--- web/app/themes/theme-wordpress/single.php
+++ web/app/themes/theme-wordpress/single.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
 	<main>
-		
+		<section>
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <article>
+                    <h1><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                </article>
+            <?php endwhile; else : ?>
+                <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
+        </section>
 	</main>
 <?php get_footer(); ?>
