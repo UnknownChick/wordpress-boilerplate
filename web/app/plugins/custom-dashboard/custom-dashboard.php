6b798373f0850862c39ef00d96926a9e7652860b
Alexandre <alexandre.ferreira7@proton.me>
2024-12-07 20:44:19 +0100

    test(custom dashboard plugin): separate css in multiple files

--- web/app/plugins/custom-dashboard/custom-dashboard.php
+++ web/app/plugins/custom-dashboard/custom-dashboard.php
@@ -7,5 +7,8 @@ Author: Alexandre Ferreira
 */
 
 add_action('admin_enqueue_scripts', function () {
-    wp_enqueue_style('custom-dashboard-style', plugins_url('css/dashboard-style.css', __FILE__));
+    wp_enqueue_style('dashboard-buttons', plugins_url('css/buttons.css', __FILE__));
+    wp_enqueue_style('dashboard-forms', plugins_url('css/forms.css', __FILE__));
+    wp_enqueue_style('dashboard-style', plugins_url('css/style.css', __FILE__));
+    wp_enqueue_style('dashboard-tables', plugins_url('css/tables', __FILE__));
 });
