f8f029fa9a526200e0d2f1f0825fcae49891b1b3
Alexandre <35708209+UnknownChick@users.noreply.github.com>
2025-02-17 00:58:50 +0100

    feat(core): create FileAutoload class to import files

--- web/app/themes/default/functions.php
+++ web/app/themes/default/functions.php
@@ -6,33 +6,27 @@ defined('ABSPATH') || die();
 
 use Carbon_Fields\Carbon_Fields;
 use App\Helpers\SmtpHelper;
+use App\Utils\FileAutoload;
+use Timber\Timber;
 
 add_action('after_setup_theme', function () {
     Carbon_Fields::boot();
     SmtpHelper::init();
+    Timber::init();
 });
 
-$roots_includes = array(
-    '/inc/admin/pages/page-options.php',
-    '/inc/admin/capabilities.php',
-    '/inc/admin/cleanup.php',
-    '/inc/admin/duplicate-post.php',
-    '/inc/admin/footer.php',
-
-    '/inc/shortcodes/site-map.php',
-
-    '/inc/core.php',
-    '/inc/optimizations.php',
-    '/inc/scripts.php',
-    '/inc/security.php',
-);
-
-foreach ($roots_includes as $file) {
-    $filepath = locate_template($file);
-    if ($filepath && file_exists($filepath)) {
-        require_once $filepath;
-    } else {
-        trigger_error("Error locating `$file` for inclusion!", E_USER_ERROR);
-    }
-}
-unset($file, $filepath);
+$fileAutoload = FileAutoload::getInstance();
+$fileAutoload->addFiles('admin', [
+    'pages/page-options.php',
+    'capabilities.php',
+    'cleanup.php',
+    'duplicate-post.php',
+    'footer.php',
+]);
+$fileAutoload->addFiles('', [
+    'core.php',
+    'optimizations.php',
+    'scripts.php',
+    'security.php',
+]);
+$fileAutoload->load();
