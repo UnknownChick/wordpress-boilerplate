ce8104ec23a4650b924c50f4efc24a988202b88d
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2025-10-26 02:55:48 +0200

    refactor: remove Carbon Fields references and clean up file loading in functions.php

--- web/app/themes/default/functions.php
+++ web/app/themes/default/functions.php
@@ -4,20 +4,18 @@ namespace App;
 
 defined('ABSPATH') || die();
 
-//use Carbon_Fields\Carbon_Fields;
 use App\Helpers\SmtpHelper;
 use App\Utils\FileAutoload;
 use Timber\Timber;
 
 add_action('after_setup_theme', function () {
-    //Carbon_Fields::boot();
     SmtpHelper::init();
     Timber::init();
 });
 
 $fileAutoload = FileAutoload::getInstance();
 $fileAutoload->addFiles('admin', [
-    //'pages/page-options.php',
+    'pages/page-options.php',
     'capabilities.php',
     'cleanup.php',
     'duplicate-post.php',
