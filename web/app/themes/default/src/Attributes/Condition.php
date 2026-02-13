09395abd6a298191d72460a399c0546daf7ea006
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2026-02-13 01:33:18 +0100

    refactor: enhance Condition class to support multiple arguments in evaluation

--- web/app/themes/default/src/Attributes/Condition.php
+++ web/app/themes/default/src/Attributes/Condition.php
@@ -9,12 +9,18 @@ defined('ABSPATH') || die();
  *
  * Usage :
  *   #[Condition('is_admin')]
- *   #[Condition('is_user_logged_in')]
+ *   #[Condition('function_exists', ['register_extended_field_group'])]
  */
-#[\Attribute(\Attribute::TARGET_CLASS)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::IS_REPEATABLE)]
 class Condition
 {
-    public function __construct(
-        public readonly string $fn,
-    ) {}
+	public function __construct(
+		public readonly string $fn,
+		public readonly array $args = [],
+	) {}
+
+	public function evaluate(): bool
+	{
+		return (bool) call_user_func($this->fn, ...$this->args);
+	}
 }
