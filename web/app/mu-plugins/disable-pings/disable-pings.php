7c38a1f9392f3ff9bc2fa86331d115d4c4bda8ff
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2026-03-01 18:38:14 +0100

    refactor: update plugin description and enhance self-ping functionality with improved code structure

--- web/app/mu-plugins/disable-pings/disable-pings.php
+++ web/app/mu-plugins/disable-pings/disable-pings.php
@@ -7,25 +7,38 @@
  * @wordpress-plugin
  * Plugin Name: Disable Pings
  * Plugin URI: https://alexandre-ferreira.fr
- * Description: Disable pings and trackbacks
- * Version: 1.0
+ * Description: Disable self-pings, pingbacks, and trackbacks
+ * Version: 1.1
  * Author: Alexandre Ferreira
  * Author URI: https://alexandre-ferreira.fr
  * Text Domain: alexandre
  * License: MIT License
  */
 
-
 /**
- * Disable self trackback for the given links array.
+ * Disable self-pings by removing internal URLs from the links array.
  *
- * @param array $links The array of links.
+ * @param array $links The array of links to ping.
  * @return void
  */
-add_action('pre_ping', function (array &$links) {
-    foreach ($links as $l => $link) {
-        if (str_starts_with($link, get_option('home'))) {
-            unset($links[$l]);
-        }
-    }
+add_action('pre_ping', function (array &$links): void {
+	$home = get_option('home');
+	$links = array_filter($links, fn(string $link): bool => !str_starts_with($link, $home));
 });
+
+/**
+ * Disable XML-RPC pingback method to prevent abuse.
+ *
+ * @param array $methods The available XML-RPC methods.
+ * @return array The filtered methods.
+ */
+add_filter('xmlrpc_methods', function (array $methods): array {
+	unset($methods['pingback.ping'], $methods['pingback.extensions.getPingbacks']);
+	return $methods;
+});
+
+/**
+ * Disable pingback and trackback functionality entirely.
+ */
+add_filter('xmlrpc_enabled', '__return_false');
+add_filter('pings_open', '__return_false', 20, 2);
