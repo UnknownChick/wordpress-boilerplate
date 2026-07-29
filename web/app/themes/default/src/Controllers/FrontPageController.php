fb9eef49b54ba40d68c775a6bbc2d78567ff5044
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2026-07-29 23:27:20 +0200

    feat: enhance homepage layout and integrate latest posts section

--- web/app/themes/default/src/Controllers/FrontPageController.php
+++ web/app/themes/default/src/Controllers/FrontPageController.php
@@ -7,9 +7,14 @@ namespace Theme\Controllers;
 defined('ABSPATH') || die();
 
 use Theme\Core\AbstractController;
+use Theme\Repositories\PostRepository;
 
 class FrontPageController extends AbstractController
 {
+	public function __construct(private PostRepository $posts)
+	{
+	}
+
 	public function view(): string
 	{
 		return 'pages/home.twig';
@@ -18,7 +23,10 @@ class FrontPageController extends AbstractController
 	protected function data(): array
 	{
 		return [
-			// Add homepage-specific data here
+			// Cached for 15 minutes (see PostRepository::latest()): safe to
+			// use for "recent posts" style widgets that don't need to be
+			// second-by-second accurate.
+			'latest_posts' => $this->posts->latest('post', 3),
 		];
 	}
 }
