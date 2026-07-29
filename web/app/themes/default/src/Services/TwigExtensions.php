39cbf5b04c921f502617122bcd39cd004161cfcb
UnknownChick <35708209+UnknownChick@users.noreply.github.com>
2026-07-29 23:26:52 +0200

    feat: add WordPress translation helpers to Twig for direct usage in views

--- web/app/themes/default/src/Services/TwigExtensions.php
+++ web/app/themes/default/src/Services/TwigExtensions.php
@@ -26,6 +26,14 @@ class TwigExtensions implements Registerable
 	{
 		$twig->addFunction(new TwigFunction('asset', [$this, 'asset']));
 
+		// WordPress translation helpers, usable directly in Twig views
+		// (e.g. {{ __('Hello', 'default') }}), since Timber v2 no longer
+		// registers them by default.
+		$twig->addFunction(new TwigFunction('__', '__'));
+		$twig->addFunction(new TwigFunction('_e', '_e'));
+		$twig->addFunction(new TwigFunction('_n', '_n'));
+		$twig->addFunction(new TwigFunction('_x', '_x'));
+
 		return $twig;
 	}
 
